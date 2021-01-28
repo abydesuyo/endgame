fbfbd04393c32b24f7ba675c66ffdecb2f2b087a
Aby <aby.pzcrazy@gmail.com>
2021-01-28 23:56:50 +0900

    Changes to JS, description etc

--- js/scripts.js
+++ js/scripts.js
@@ -211,14 +211,15 @@ $(document).ready(function () {
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
-
+        var tempvar = $('#invite_code').val();
+        // $('#alert-wrapper').html(alert_markup('info', `Using code ${tempvar}`));
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '1b4b16ae294b41580302d0090a902233'
+            || MD5($('#invite_code').val()) !== '1b4b16ae294b41580302d0090a902233') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyYbKvXmV1KSqEIWw3Tj9swjdj_MObNNX618qH9cshuSrZhQnLidM22/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
